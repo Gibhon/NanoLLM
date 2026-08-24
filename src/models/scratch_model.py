87a047f896706f2dc049c07ba6e33d89b8ce8238
Gibhon <gibhonofficial@gmail.com>
2026-08-24 21:35:51 +0545

    modification: Deleted Redundant comment

--- src/models/scratch_model.py
+++ src/models/scratch_model.py
@@ -4,45 +4,6 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-#
-# -------This Works but is too memory inefficient :(-------
-#
-# class TransformerEmbeddingScratch(nn.Module):
-#     def __init__(self, vocab_size, embed_dim, max_seq_len):
-#         self.vocab_size = vocab_size
-#         self.max_seq_len = max_seq_len
-
-#         self.token_embed = nn.Linear(vocab_size, embed_dim, bias=False)
-#         self.pos_embed = nn.Linear(max_seq_len, embed_dim, bias=False)
-
-#     def forward(self, x):
-#         seq_len = x.size(1)
-
-#         # Shape: (batch_size, seq_len) -> (batch_size, seq_len, vocab_size)
-#         # [1, 2, 3] -> [[1, 0, 0], [0, 1, 0], [0, 0, 1]]   || (3) -> (3, 3) HERE: Vocab in place of 3
-#         x_one_hot = F.one_hot(x, num_classes=self.vocab_size).float()
-
-#         positions = torch.arange(0, seq_len, device=x.device)
-
-#         # TODO: Understand the Query
-#         # ? Why max_seq_len when positions is only till seq_len anyways
-#         # Shape: (seq_len) -> (seq_len, max_seq_len)
-#         pos_onehot = F.one_hot(
-#             positions,
-#             num_classes=self.max_seq_len,
-#         ).float()
-
-#         # (seq_len, self.max_seq_len) -> (1, seq_len, max_seq_len)
-#         pos_onehot = pos_onehot.unsqueeze(0)
-
-#         # (batch_size, seq_len, vocab_size) @ (vocab, embed) -> (batch, seq, embed)
-#         token_embedding = self.token_embed(x_one_hot)
-#         # (1, seq, max) @ (max, embed) -> (1, seq, embed)
-#         pos_embedding = self.pos_embed(pos_onehot)
-
-#         # 1 is broadcasted to batch_size
-#         return token_embedding + pos_embedding
-
 
 class TransformerEmbeddingScratch(nn.Module):
     def __init__(self, embed_dim, vocab_size, max_seq_len):
